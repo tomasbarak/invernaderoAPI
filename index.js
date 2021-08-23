b5567951f542831ce9a25ca0e510572999b7af88
tomasbarak <barakcinthia@gmail.com>
2021-08-23 13:38:55 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const http = require("http");
+
+const handleError = (err, res) => {
+    res
+        .status(500)
+        .contentType("text/plain")
+        .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+    dest: "/temp"
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
+
+
+app.post(
+    "/uploads",
+    upload.single("file" /* name attribute of <file> element in your form */),
+    (req, res) => {
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, "./uploaded/image.png");
+
+        if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+            fs.rename(tempPath, targetPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(200)
+                    .contentType("text/plain")
+                    .end("File uploaded!");
+            });
+        } else {
+            fs.unlink(tempPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(403)
+                    .contentType("text/plain")
+                    .end("Only .png files are allowed!");
+            });
+        }
+    }
+);
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
